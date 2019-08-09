af281726e8a88850ce6df3272ae1b9c48e382131
Ola Siem <info@siemieniuk.net>
2019-08-09 14:50:25 +0200

    Get 'href' attribute of the clicked link

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ document.getElementById('test-button').addEventListener('click', function(){
 
 
 const titleClickHandler = function(event){
+  event.preventDefault();
   const clickedElement = this;
   console.log('Link was clicked!');
 
@@ -33,11 +34,16 @@ const titleClickHandler = function(event){
     activeArticle.classList.remove('active');
   }
 
-  /* get 'href' attribute from the clicked link */
+  /* [DONE] get 'href' attribute from the clicked link */
+
+  const articleSelector = clickedElement.getAttribute("href");
+
+  console.log(articleSelector);
 
   /* find the correct article using the selector (value of 'href' attribute) */
 
   /* add class 'active' to the correct article */
+
 }
 
 const links = document.querySelectorAll('.titles a');
